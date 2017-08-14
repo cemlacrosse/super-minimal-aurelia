d37ea334e024d170b188b95d4e9c64cb15e0c798
Matt LaCrosse <mlacrosse@clubsoftinc.com>
2017-08-14 13:51:03 -0500

    upated packages

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ export class AppViewModel {
     this.router = router;
     config.map([
       { route: '', name: 'hello', moduleId: PLATFORM.moduleName('page-hello', 'hello') },
-      { route: 'about', name: 'about', moduleId: PLATFORM.moduleName('page-about', 'about') },
+      { route: 'about', name: 'about', moduleId: PLATFORM.moduleName('page-about', 'about') }
     ]);
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
 
   entry: {
-    main: [ 
+    main: [
       './ie-polyfill',
       'aurelia-bootstrapper'
     ]
@@ -17,18 +17,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: '[name].js',    
+    filename: '[name].js',
     chunkFilename: '[name].js'
   },
 
   resolve: {
     extensions: ['.js'],
-    modules: ['src', 'node_modules'],
+    modules: ['src', 'node_modules']
   },
 
   module: {
     rules: [
-      { test: /\.(js)$/,
+      {
+        test: /\.(js)$/,
         loaders: 'babel-loader',
         exclude: /node_modules/,
         query: { // use 'babel-preset-env' without transforming ES6 modules, and with added support for decorators
@@ -40,8 +41,8 @@ module.exports = {
         test: /\.css$/,
         exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
+          fallback: 'style-loader',
+          use: 'css-loader'
         })
       },
       {
@@ -51,30 +52,30 @@ module.exports = {
           use: ['css-loader', 'sass-loader']
         })
       },
-      { test: /\.html$/i, 
+      { test: /\.html$/i,
         use: 'html-loader' }
     ]
-  },  
+  },
 
   plugins: [
 
     // required polyfills for non-evergreen browsers
-    new webpack.ProvidePlugin({        
-        Map: 'core-js/es6/map',
-        WeakMap: 'core-js/es6/weak-map',
-        Promise: 'core-js/es6/promise',
-        regeneratorRuntime: 'regenerator-runtime' // to support await/async syntax
+    new webpack.ProvidePlugin({
+      Map: 'core-js/es6/map',
+      WeakMap: 'core-js/es6/weak-map',
+      Promise: 'core-js/es6/promise',
+      regeneratorRuntime: 'regenerator-runtime' // to support await/async syntax
     }),
 
     // init aurelia-webpack-plugin
     new AureliaWebpackPlugin.AureliaPlugin(),
-    
-    new ExtractTextPlugin({filename: "styles.css", disable: false, allChunks: true}),
+
+    new ExtractTextPlugin({filename: 'styles.css', disable: false, allChunks: true}),
 
     // have Webpack copy over the index.html and inject appropriate script tag for Webpack-bundled entry point 'main.js'
     new HtmlWebpackPlugin({
-        template: '!html-webpack-plugin/lib/loader!index.html',
-        filename: 'index.html'
+      template: '!html-webpack-plugin/lib/loader!index.html',
+      filename: 'index.html'
     })
   ]
 };
